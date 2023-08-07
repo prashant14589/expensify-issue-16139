950530f2b447beee0a8d2112f23e3c15db2da907
Prashant Kumar <prashant07303@gmail.com>
2023-08-08 01:42:17 +0530

    change class based component to functional

--- src/components/DeeplinkWrapper/index.js
+++ src/components/DeeplinkWrapper/index.js
@@ -1,15 +1,12 @@
 import PropTypes from 'prop-types';
-import {PureComponent} from 'react';
 
 const propTypes = {
     /** Children to render. */
     children: PropTypes.node.isRequired,
 };
 
-class DeeplinkWrapper extends PureComponent {
-    render() {
-        return this.props.children;
-    }
+function DeeplinkWrapper({ children }) {
+    return children;
 }
 
 DeeplinkWrapper.propTypes = propTypes;
